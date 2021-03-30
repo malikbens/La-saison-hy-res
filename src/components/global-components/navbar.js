68f04405994e84106af2cb642849f4197a8677f4
Malik Bens <malikbens@live.fr>
2021-03-30 18:16:49 +0200

    first commit

---
+++ src/components/global-components/navbar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+class Navbar extends Component {
+
+  render() {
+    let publicUrl = process.env.PUBLIC_URL + '/'
+    let imgattr = 'logo'
+    let anchor = '#'
+    return (
+      <div>
+        <div className="navbar-area">
+          <nav className="navbar navbar-area navbar-expand-lg">
+            <div className="container nav-container">
+              <div className="responsive-mobile-menu">
+                <button className="menu toggle-btn d-block d-lg-none" data-toggle="collapse" data-target="#realdeal_main_menu" aria-expanded="false" aria-label="Toggle navigation">
+                  <span className="icon-left" />
+                  <span className="icon-right" />
+                </button>
+              </div>
+              <div className="logo readeal-top">
+                LOGO
+                    </div>
+              <div className="nav-right-part nav-right-part-mobile">
+                <Link className="btn btn-yellow" to="/add-property">ADD LISTING <span className="right"><i className="la la-plus" /></span></Link>
+              </div>
+              <div className="collapse navbar-collapse" id="realdeal_main_menu">
+                <ul className="navbar-nav menu-open readeal-top">
+                  <li><Link to="/home-v2">Acceuil</Link></li>
+                  <li><Link to="/search-list">Annonces</Link></li>
+
+                  <li className="menu-item-has-children">
+                    <a href="#">Services</a>
+                    <ul className="sub-menu">
+                      <li><Link to="/About">A propos</Link></li>
+                      <li><Link to="/advisor">Partenaires</Link></li>
+
+                    </ul>
+                  </li>
+
+                  <li><Link to="/contact">Contact</Link></li>
+                </ul>
+              </div>
+
+            </div>
+          </nav>
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,17 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter as Router, HashRouter, Route, Switch } from "react-router-dom";
+import blogdata from './data/blogdata.json';
+import Singleblogdata from './data/single-blogdata.json';
+import HomeV1 from './components/home-v1';
+import HomeV2 from './components/home-v2';
+import HomeV3 from './components/home-v3';
+import HomeV4 from './components/home-v4';
+import Property from './components/property';
+import AvilableProperty from './components/availavbe-property';
+import PropertiesByCity from './components/properties-by-city';
+import RecentProperties from './components/recent-properties';
+import PropertyDetails from './components/property-details';
+import About from './components/about';
+import Advisor from './components/advisor';
+import Pricing from './components/pricing';
+import UserList from './components/user-list';
+import Registraion from './components/registration';
+import Error from './components/error';
+import Faq from './components/faq';
+import News from './components/news';
+import NewsDetails from './components/news-details';
+import Contact from './components/contact';
+import SearchMap from './components/search-map';
+import SearchGrid from './components/search-grid';
+import SearchList from './components/search-list';
+import AddNew from './components/add-property';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+class Root extends Component {
+    render() {
+        return(
+            <Router>
+                <HashRouter basename="/">
+                <div>
+                <Switch>
+                    <Route exact path="/" component={HomeV1} />
+                    <Route path="/home-v2" component={HomeV2} />
+                    <Route path="/home-v3" component={HomeV3} />
+                    <Route path="/home-v4" component={HomeV4} />
+                    <Route path="/property" component={Property} />
+                    <Route path="/availavbe-property" component={AvilableProperty} />
+                    <Route path="/properties-by-city" component={PropertiesByCity} />
+                    <Route path="/recent-properties" component={RecentProperties} />
+                    <Route path="/property-details" component={PropertyDetails} />
+                    <Route path="/about" component={About} />
+                    <Route path="/advisor" component={Advisor} />
+                    <Route path="/pricing" component={Pricing} />
+                    <Route path="/user-list" component={UserList} />
+                    <Route path="/registration" component={Registraion} />
+                    <Route path="/error" component={Error} />
+                    <Route path="/faq" component={Faq} />
+                    <Route path="/news" component={News} />
+                    <Route path="/news-details" component={NewsDetails} />
+                    <Route path="/contact" component={Contact} />
+                    <Route path="/search-map" component={SearchMap} />
+                    <Route path="/search-grid" component={SearchGrid} />
+                    <Route path="/search-list" component={SearchList} />
+                    <Route path="/add-property" component={AddNew} />
+                </Switch>
+                </div>
+                </HashRouter>
+            </Router>
+        )
+    }
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+export default Root;
+
+ReactDOM.render(<Root />, document.getElementById('realdeal'));
